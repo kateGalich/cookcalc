432cd75451955f5b7781190e580c3c672d3e32ca
Kate Galich <kate.galich@outlook.com>
2023-03-24 22:51:46 -0400

    adding css

--- src/App.js
+++ src/App.js
@@ -8,23 +8,16 @@ function App() {
 
         <h1>How much to cook</h1>
 
-        <div>
-          <div>
+        <div class='container'>
+         
             <label for='part1'>Flour</label>
             <input type="number" id="part1"></input>
-          </div>
-          <div>
             <label for='part2'>Milk</label>
             <input type='number' id='part2'></input>
-          </div>
-          <div>
             <label for='part3'>Milk</label>
             <input type='number' id='part3'></input>
-          </div>
-          <div>
             <label for='part4'>Persons</label>
             <input type='range' min='1' max='10' step="1" value="2" id='part4'></input>
-          </div>
 
         </div>
       </header>
