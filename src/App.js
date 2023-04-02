3c76588554e3304d93b5367b0afca04f22381470
Kate Galich <kate.galich@outlook.com>
2023-04-02 18:28:04 -0400

    dynamicaly update model

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App(props) {
+  let [personCount, setPersonCount] = useState(props.persons);
   let parts = props.parts.map(p => <>
     <label for='part1'>{p.name}</label>
     <input type="number" id="part1" defaultValue={p.amount}></input>
@@ -12,11 +14,18 @@ function App(props) {
       <header className="App-header">
 
         <h1>How much to cook</h1>
-        <h2>{props.recipeName} for {props.persons}</h2>
+        <h2>{props.recipeName} for {personCount}</h2>
         <div class='container'>
           {parts}
           <label for='part4'>Persons</label>
-          <input type='range' min={props.minPersons} max={props.maxPersons} step="1" defaultValue={props.persons} id='part4'></input>
+          <input
+            type='range'
+            min={props.minPersons}
+            max={props.maxPersons}
+            step="1"
+            value={personCount}
+            onChange={e => setPersonCount(e.target.value)}
+            id='part4'></input>
 
         </div>
       </header>
