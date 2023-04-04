4170648903745df543b24dbbbecc1393e7a3f11c
Kate Galich <kate.galich@outlook.com>
2023-04-04 13:06:33 -0400

    Recalculating recepi parts based on person count

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 
 function App(props) {
   let [recipe, setRecipe] = useState(props);
 
-  function setPersons(value){
+  function setPersons(value) {
     setRecipe(oldRecipe => {
       let newRecepi = {
         ...oldRecipe,
-        persons: value
+        parts: oldRecipe.parts.map(x => ({...x})),
+        persons: value,
       };
+      for (const part of newRecepi.parts) {
+        part.amount = part.amount / oldRecipe.persons * newRecepi.persons;
+      }
       return newRecepi;
     });
   }
 
-  let parts = recipe.parts.map(p => <>
-    <label for='part1'>{p.name}</label>
-    <input type="number" id="part1" defaultValue={p.amount}></input>
-    <span>{p.unit}</span>
-  </>);
+  let parts = recipe.parts.map((p, index) =>
+    <Fragment key={index}>
+      <label htmlFor='part1'>{p.name}</label>
+      <input type="number" id="part1" value={p.amount}>
+      </input>
+      <span> {p.unit}</span>
+    </Fragment>);
   return (
     <div className="App">
       <header className="App-header">
 
         <h1>How much to cook</h1>
         <h2>{recipe.recipeName} for {recipe.persons}</h2>
-        <div class='container'>
+        <div className='container'>
           {parts}
-          <label for='part4'>Persons</label>
+          <label htmlFor='part4'>Persons</label>
           <input
             type='range'
             min={recipe.minPersons}
@@ -40,8 +46,8 @@ function App(props) {
             id='part4'></input>
 
         </div>
-      </header>
-    </div>
+      </header >
+    </div >
   );
 }
 
